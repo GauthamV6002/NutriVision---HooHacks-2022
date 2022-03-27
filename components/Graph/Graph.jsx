e973a57144f272ea863f050c59f7e120587fa917
Gautham Venkateshwaran <gauthamv6002@gmail.com>
2022-03-27 10:53:48 -0600

    Features

---
+++ components/Graph/Graph.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+export default function Graph({ data }) {
+    return (
+        <View>
+            <Text>Incomplete</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
